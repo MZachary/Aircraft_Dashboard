538e0ab0d2b52d300ea9c1f93f4ecbbca078b52e
Ryan Hill <ryan.hill@tilefive.com>
2023-02-20 00:54:53 -0600

    bootstrap is probably easiest to work with

--- app.py
+++ app.py
@@ -5,19 +5,10 @@ import serial
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
-
-
-
-
-# configuration
-DEBUG = True
-
-
 #instantiate the app
 app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
 app.config['SECRET_KEY'] = 'asdf1234'
 socketio = SocketIO(app)
-
 
 @app.route('/', methods=['GET'])
 def dashboard():
@@ -26,8 +17,6 @@ def dashboard():
 @socketio.on('my event')
 def handle_my_custom_event(json):
     print('received json: ' + str(json))
-
-
 
 if __name__ == '__main__':
     socketio.run(app)
